81b95c67e400cb96a6ba3717d5ea3c41e009007b
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-03-26 22:39:47 +0100

    Base Program working as intended
    Added the SquaredError function.
    Now the program counts exactly the number of iterations passed, all while printing all the results calculated

--- src/Controllers/CalculationController.java
+++ src/Controllers/CalculationController.java
@@ -5,29 +5,41 @@ import static Controllers.OutputController.Display;
 import static Models.DataSetModel.*;
 
 public class CalculationController {
-    private static float hp;
     private static int iterationCounter = 0;
+    private static boolean Initialised = false;
 
     public static void Start(){
-        InitialiseP0AndP1();
-        CalculatePredictedValues();
-
         Display("Input: " + Arrays.toString(GetInputDataSet()));
         Display("Output: " + Arrays.toString(GetOutputDataSet()));
-        Display(Arrays.toString(GetPredictedData(iterationCounter)));
 
-        AddP0AndP1(CorrectP(0), CorrectP(1));
-        iterationCounter++;
+        int i = 3;
+        while (i > 0) {
+            i--;
+            if (Initialised) {
+                UpdateP0AndP1();
+                iterationCounter++;
+            } else
+                InitialiseP0AndP1();
 
-        Display(Arrays.toString(GetP0AndP1(iterationCounter)));
+            Display(Arrays.toString(GetP0AndP1(iterationCounter)));
 
-        CalculateSquaredError();
+            CalculatePredictedValues();
+            Display(Arrays.toString(GetPredictedData(iterationCounter)));
 
+            CalculateSquaredError();
+            Display("Jp : " + (GetSquaredError(iterationCounter)));
+
+        }
+        Display("Iteration Count: " + iterationCounter);
     }
 
     private static void InitialiseP0AndP1() {
         // Get a random value for P1 and P0
         AddP0AndP1(0, 0);
+        Initialised = true;
+    }
+    private static void UpdateP0AndP1() {
+        AddP0AndP1(CorrectP(0), CorrectP(1));
     }
     private static void CalculatePredictedValues() {
         int[] X = GetInputDataSet();
@@ -52,8 +64,18 @@ public class CalculationController {
         P -= (0.01 /M) * sigma;
         return P;
     }
-    private static void CalculateSquaredError() { // Jp = (1/2M) * Sigma[(Hp(x_i) - y_i)²]
+    private static void CalculateSquaredError() { // Jp = (1/2M) * Sigma[(Hp(x_i) - y_i)²] = 4.3167
+        float SquaredError;
+        float sigma = 0;
+        float[] Hp = GetPredictedData(iterationCounter);
+        int[] Y = GetOutputDataSet();
+        int M = Y.length;
 
+        for (int i = 0; i < M; i++) {
+            sigma += Math.pow(Hp[i] - Y[i], 2);
+        }
+        float M2 = 2 * M;
+        SquaredError = (float) ((1.0 /M2) * sigma);
+        AddSquaredError(SquaredError);
     }
-
 }
--- src/Models/DataSetModel.java
+++ src/Models/DataSetModel.java
@@ -7,7 +7,8 @@ public class DataSetModel {
     private static final int[] dataSet_X = new int[]{1, 2, 4};
     private static final int[] dataSet_Y = new int[]{2, 1, 5};
     private static final List<float[]> predictedData = new ArrayList<>();
-    private static List<float[]> P = new ArrayList<>();
+    private static final List<float[]> P = new ArrayList<>();
+    private static final List<Float> SquaredError = new ArrayList<>();
 
     public static int[] GetInputDataSet(){ return dataSet_X; }
     public static int[] GetOutputDataSet(){ return dataSet_Y; }
@@ -17,5 +18,8 @@ public class DataSetModel {
 
     public static void AddP0AndP1(float p0, float p1){ P.add(new float[]{p0, p1});}
     public static float[] GetP0AndP1(int iteration){ return P.get(iteration);}
+
+    public static void AddSquaredError(float squaredError){ SquaredError.add(squaredError);}
+    public static float GetSquaredError(int iteration){ return SquaredError.get(iteration);}
 
 }
